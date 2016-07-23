291d5a5cf37754ebcf4ec3fc92447dffbf2f6852
Jordan Patterson <jordan@jpatterson.me>
2016-07-23 15:03:30 -0400

    live twitter widget

--- js/main.js
+++ js/main.js
@@ -103,4 +103,9 @@ $(function() {
       }
     }
   });
+
+  $('.twitter-wrap').on('DOMSubtreeModified propertychange',"#twitter-widget-0", function() {
+    $(".twitter-timeline").contents().find(".timeline-Tweet-media").css("display", "none");
+    $(".twitter-block").css("height", "100%");
+  });
 });
